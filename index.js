3afdcf27d093f1996b858861902054dff3a7f67f
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-07-02 00:59:42 +0600

    Checking in frontend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const axios = require("axios");
 // Configure CORS
 const corsOptions = {
   // origin: "http://localhost:5173", // Replace with your frontend domain
-  origin: "https://bobbili-urban-development-authority.netlify.app/", // Replace with your frontend domain
+  origin: "https://bobbili-urban-development-authority.netlify.app", // Replace with your frontend domain
   credentials: true, // This is important to allow cookies to be sent
 };
 
