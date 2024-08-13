d4700b7c620b22d0062d9ed1ec0688d4609dfe12
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2024-08-13 23:46:49 +0600

    Cors update

--- Src/Configs/CorsOptions.js
+++ Src/Configs/CorsOptions.js
@@ -1,6 +1,6 @@
 const corsOptions = {
-  // origin: "http://localhost:5173", // Replace with your frontend domain
-  origin: "https://bobbili-urban-development-authority.netlify.app",
+  origin: "http://localhost:5173", // Replace with your frontend domain
+  // origin: "https://bobbili-urban-development-authority.netlify.app",
   // Replace with your frontend domain
   credentials: true, // This is important to allow cookies to be sent
 };
